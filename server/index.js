2bfe4d6b3f594ba294b4e78451f50c75b943b056
my3rdstory <my3rdstory@gmail.com>
2026-02-12 12:22:12 +0900

    설치 완료 콘솔 메시지의 외부 링크 안내 제거
    서버 시작 시 출력되는 GitHub README 참고 문구를 삭제
    
    설치/운영 핵심 안내만 남겨 문의성 안내 없이 종료 메시지를 단순화

--- server/index.js
+++ server/index.js
@@ -688,9 +688,6 @@ app.listen(PORT, HOST, () => {
       "- nohup(백그라운드):",
       `  cd ${ROOT}`,
       `  nohup env AUTOCLAW_BIND=${HOST} AUTOCLAW_PORT=${PORT} node server/index.js > autoclaw.log 2>&1 &`,
-      "",
-      "자세한 내용은 GitHub README를 참고해 주세요:",
-      "https://github.com/demolabskr/openclaw-study-install",
       hr,
       "",
     ];
